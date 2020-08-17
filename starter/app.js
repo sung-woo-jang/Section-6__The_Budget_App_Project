d40be8b00fa25bd3c0cd404058e6e6d2ea39d8d4
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2020-08-17 14:48:17 +0900

    ~Creating Income and Expense Function Constructors

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,57 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
-    // Some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: [],
+        },
+        totals: {
+            exp: 0,
+            inc: 0,
+        },
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        },
+    };
 })();
 
 // UI CONTOLLER
@@ -17,7 +68,7 @@ var UIController = (function () {
             return {
                 // value : input 요소에서는 단순히 입력된 값을 얻지만
                 // select요소에서는 option의 value값을 얻는다
-                type: document.querySelector(DOMstrings.inputType).value, //비용 지출에 대한 지출 또는 지출이 있을 것이다.???
+                type: document.querySelector(DOMstrings.inputType).value, //  Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription)
                     .value,
                 value: document.querySelector(DOMstrings.inputValue).value,
@@ -31,23 +82,43 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDomstrings();
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDomstrings();
+
+        document
+            .querySelector(DOM.inputBtn)
+            .addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
+        var input, newItem;
+
         // 1. 입력된 데이터 가져오기
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
+
         // 2. 예산 관리자에 항목 추가
+        newItem = budgetCtrl.addItem(
+            input.type,
+            input.description,
+            input.value
+        );
         // 3. UI에 항목 추가
         // 4. 예산 계산
         // 5. UI에 예산 표시
     };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
+        },
+    };
 })(budgetController, UIController);
+
+controller.init();
